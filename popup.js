cbec9d03eaab2d18e2677352db579f419ca818fc
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-07-24 15:01:10 +0100

    Initial outline

---
+++ popup.js
@@ -0,0 +1,14 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+document.querySelector('#go-to-options').addEventListener(function() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+});
