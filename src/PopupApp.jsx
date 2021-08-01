28e27f6018d7e0ba8385a9289030230f58ef1ce6
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-08-01 02:13:28 +0100

    Adding theme

--- src/PopupApp.jsx
+++ src/PopupApp.jsx
@@ -5,14 +5,42 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import blueGrey from '@material-ui/core/colors/blueGrey'
+import green from '@material-ui/core/colors/green'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[700]
+    },
+    secondary: {
+      main: blueGrey[700]
+    },
+    outline: {
+      main: blueGrey[50]
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        },
+      },
+    },
+  }
+})
 
 const PopupApp = () => {
   return (
-    <div className='App'>
-
-      <Button variant="text">Close</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Button variant='text'>Close</Button>
+    </ThemeProvider>
   )
 }
 
